2d0076407fc22d0ae00e81a4f62562171cac4ac4
Daniel Berndt <daniel.berndt@gmail.com>
2017-08-10 16:00:12 +0200

    Add is_private field to placement_scores

--- db/initial/schemas/placement_scores.sql
+++ db/initial/schemas/placement_scores.sql
@@ -3,6 +3,7 @@ create table placement_scores (
   score float not null,
   decay_rate float not null,
   constant_until timestamp with time zone not null default now(),
+  is_private boolean not null,
 
   creator_id int not null references users(id),
   created_at timestamp with time zone not null default now()
