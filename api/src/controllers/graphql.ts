3a9b3e9b3acdb54c4d3e3a86f2ccbe30a62ed755
Daniel Berndt <daniel.berndt@gmail.com>
2018-08-15 14:39:14 +0200

    replace express graphql plugin with custom fastify graphql adapter (including cache!)

--- api/src/controllers/graphql.ts
+++ api/src/controllers/graphql.ts
@@ -9,17 +9,23 @@ import {
   GraphQLInt,
   GraphQLList,
   GraphQLNonNull,
-  formatError,
-  GraphQLError,
+  Source,
+  validateSchema,
+  parse,
+  DocumentNode,
+  validate,
+  specifiedRules,
+  execute,
 } from "graphql";
-const graphqlHTTP = require("express-graphql");
-const fpPlugin = require("fastify-plugin");
 import {TeamIntegration} from "../models/Integration";
 import {DbClient} from "types";
-import {enhancedQuery} from "../lib/query-logger";
 import {TopPlacements} from "../models/Placements";
 import {User} from "../models/User";
 import Mutations from "../mutations";
+import connectPg from "../middlewares/pg";
+import * as LRU from "lru-cache";
+import {createHash} from "crypto";
+import {resolveGraphiQLString} from "apollo-server-module-graphiql";
 
 export type Ctx = {db: DbClient; currentUserId: number | null};
 
@@ -72,32 +78,78 @@ export const QueryRoot = new GraphQLObjectType({
 
 const schema = new GraphQLSchema({query: QueryRoot, mutation: Mutations});
 
+const hashQuery = (query: string) =>
+  createHash("sha1")
+    .update(query)
+    .digest("base64");
+
 const graphqlController: fastify.Plugin<Server, IncomingMessage, ServerResponse, {}> = async (
   instance,
   options,
   next
 ) => {
-  instance.use(
-    "/graphql",
-    graphqlHTTP(async (req: any) => {
-      const auth = await getUserIdFromToken(instance.pg.query, req.headers["x-auth-token"]);
-      const context = {
-        currentUserId: auth.ok ? auth.data.userId : null,
-        db: enhancedQuery(console.log, instance.pg.query),
-      };
-      return {
+  const schemaValidationErrors = validateSchema(schema);
+  if (schemaValidationErrors.length > 0) {
+    throw new Error(schemaValidationErrors.map(e => e.message).join(".\n"));
+  }
+
+  connectPg(instance);
+  type CacheVal = {size: number; node: DocumentNode};
+  const cache = LRU<string, CacheVal>({max: 500 * 1024, length: e => e.size});
+
+  instance.post("/graphql", async req => {
+    const auth = await getUserIdFromToken(instance.pg.query, req.headers["x-auth-token"]);
+    const context = {
+      currentUserId: auth.ok ? auth.data.userId : null,
+      db: req.db,
+    };
+    const {query, variables, operationName} = req.body;
+    if (!query) return Promise.reject({status: 400, error: "Need 'query' parameter"});
+    const source = new Source(query, "GraphQL request");
+    const queryHash = hashQuery(query);
+    const cacheVal = cache.get(queryHash);
+    let documentAST: DocumentNode;
+    if (!cacheVal) {
+      try {
+        documentAST = parse(source);
+      } catch (syntaxError) {
+        return Promise.reject({status: 400, error: syntaxError});
+      }
+      const validationErrors = validate(schema, documentAST, specifiedRules);
+      if (validationErrors.length > 0) {
+        return Promise.reject({status: 400, error: validationErrors});
+      }
+      cache.set(queryHash, {size: query.length, node: documentAST});
+    } else {
+      documentAST = cacheVal.node;
+    }
+    try {
+      return execute(
         schema,
-        graphiql: true,
+        documentAST,
+        undefined, // rootValue
         context,
-        formatError: (e: GraphQLError) => {
-          console.error(e);
-          return formatError(e);
-        },
-      };
-    })
-  );
+        variables,
+        operationName
+      );
+    } catch (contextError) {
+      return Promise.reject({status: 400, error: contextError});
+    }
+  });
+
+  instance.get("/graphiql", async (req, res) => {
+    try {
+      const {query} = req.params;
+      const graphiqlString = await resolveGraphiQLString(query, {
+        endpointURL: "/graphql",
+      });
+      res.type("text/html").send(graphiqlString);
+    } catch (error) {
+      return Promise.reject({status: 500, error});
+    }
+  });
 
   if (next) next();
 };
 
-export default fpPlugin(graphqlController);
+export default graphqlController;
